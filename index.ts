2a8aaff723b87077c9450a6c683440bbeb76cb07
sachuabraham <sachu.kothanil@gmail.com>
2021-09-29 21:14:09 +0530

    function to fetch block txs

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log('hey');
+/**
+ * Starter script to find the transactions
+ */
+
+import {
+  getBlockHashFromBlockNumber,
+  getBlockTxsFromBlockHash,
+} from './src/bitcoin_apis';
+import {BLOCK_NUMBER} from './src/types';
+
+
+/**
+ * Function to find ancestry list from a bitcoin block
+ * @param {BLOCK_NUMBER}blockNumber
+ */
+async function findAncestry(blockNumber: BLOCK_NUMBER) {
+  const blockHash = await getBlockHashFromBlockNumber(blockNumber);
+  const getBlockTxs = await getBlockTxsFromBlockHash(blockHash, 0);
+  console.log(getBlockTxs);
+}
+
+
+findAncestry(680000);
--- src/bitcoin_apis.ts
+++ src/bitcoin_apis.ts
@@ -3,7 +3,7 @@
  */
 
 import {BITCOIN_UTILITY_API_URL} from './constants';
-import {AXIOS_CONFIG} from './types';
+import {AXIOS_CONFIG, BLOCK_HASH} from './types';
 import {doGetApiCall} from './utils/rest_api_caller';
 
 /**
@@ -15,10 +15,48 @@ export async function getBlockHashFromBlockNumber(
     blockHeight : number,
     apiUrl = BITCOIN_UTILITY_API_URL,
 ) {
-  const config:AXIOS_CONFIG = {
-    method: 'get',
-    url: `${apiUrl}/block-height/${blockHeight}`,
-    headers: { },
-  };
-  return await doGetApiCall(config);
+  try {
+    const config:AXIOS_CONFIG = {
+      method: 'get',
+      url: `${apiUrl}/block-height/${blockHeight}`,
+      headers: { },
+    };
+    const hash = await doGetApiCall(config);
+    return hash;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 }
+
+
+/**
+ * Function to get block txs from a specif index
+ * @dev Kindly note the API return 25 txs at a time
+ * @param {BLOCK_HASH} blockHash
+ * @param {number} index
+ * @param {string} apiUrl
+ */
+export async function getBlockTxsFromBlockHash(
+    blockHash: BLOCK_HASH,
+    index = 0,
+    apiUrl = BITCOIN_UTILITY_API_URL,
+) {
+  try {
+    const config:AXIOS_CONFIG = {
+      method: 'get',
+      url: `${apiUrl}/block/${blockHash}/txs/${index}`,
+      headers: { },
+    };
+    const txs = await doGetApiCall(config);
+
+    return {
+      txs,
+      count: txs.length,
+    };
+  } catch (err:any) {
+    console.log(err.data);
+    return false;
+  }
+}
+
