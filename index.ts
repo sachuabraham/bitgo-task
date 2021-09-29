b5340c6a538879acbd90226b719a4d93fea1de34
sachuabraham <sachu.kothanil@gmail.com>
2021-09-29 21:27:37 +0530

    fetch all transactions iteratively

--- index.ts
+++ index.ts
@@ -6,8 +6,11 @@ import {
   getBlockHashFromBlockNumber,
   getBlockTxsFromBlockHash,
 } from './src/bitcoin_apis';
-import {BLOCK_NUMBER} from './src/types';
+import {BLOCK_NUMBER, BLOCK_TXS_RESPONSE} from './src/types';
 
+// We can use redis / any other persistance layer in place here
+// in order to bypass rate limiting issues
+const cache:any = [];
 
 /**
  * Function to find ancestry list from a bitcoin block
@@ -15,8 +18,21 @@ import {BLOCK_NUMBER} from './src/types';
  */
 async function findAncestry(blockNumber: BLOCK_NUMBER) {
   const blockHash = await getBlockHashFromBlockNumber(blockNumber);
-  const getBlockTxs = await getBlockTxsFromBlockHash(blockHash, 0);
-  console.log(getBlockTxs);
+  let getBlockTxs: BLOCK_TXS_RESPONSE | false;
+  let index = 0;
+
+  // Iterates through all txs in that block 25 at a time
+  do {
+    // we could use Promise.All here if we know exactly how
+    // is the rate limiting. For now calling it serially
+    getBlockTxs = await getBlockTxsFromBlockHash(blockHash, index);
+    if (getBlockTxs) {
+      cache.push(getBlockTxs.txs);
+      console.log('tx count', index);
+    }
+    index += 25;
+  } while (getBlockTxs && index < 150); // 150 just for testing purposes
+  console.log(cache);
 }
 
 
--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,7 @@ export interface AXIOS_CONFIG {
 
 export type BLOCK_NUMBER = number;
 export type BLOCK_HASH = string;
+export interface BLOCK_TXS_RESPONSE {
+  txs :any, // need to fix this
+  count: number,
+};
